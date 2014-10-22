f1f4ef13d40199886c2572204acb904a199cdeb1
wduntak <wduntak@gmail.com>
2014-10-22 15:04:52 -0400

    Create Person class with attribute name and initialize

--- people.rb
+++ people.rb
@@ -1,11 +1,20 @@
-class Student
-	def learn 
-		"I get it!"
+class Person
+	attr_accessor :name
+
+	class Student
+		def learn 
+			"I get it!"
+		end
+	end
+
+	class Instructor
+		def teach
+			"Everything in Ruby is an Object"
+		end
 	end
 end
 
-class Instructor
-	def teach
-		"Everything in Ruby is an Object"
-	end
-end
+person = Person.new
+person.name = "Wangdu"
+
+puts person.name
