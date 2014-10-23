6e4b33efc1fcd64ba122420f4be1dbaad39e6d56
wduntak <wduntak@gmail.com>
2014-10-23 11:00:14 -0400

    Added Chris and Christina with a greeting statement

--- people.rb
+++ people.rb
@@ -1,20 +1,32 @@
 class Person
 	attr_accessor :name
 
-	class Student
+	def initialize(name)
+		@name = name
+	end
+
+	def hello
+		"Hi, my name is #{name}."
+	end
+end 
+
+	class Student < Person
+
 		def learn 
 			"I get it!"
 		end
 	end
 
-	class Instructor
+	class Instructor < Person
+
 		def teach
 			"Everything in Ruby is an Object"
 		end
 	end
-end
 
-person = Person.new
-person.name = "Wangdu"
+student = Student.new("Christina")
 
-puts person.name
+instructor = Instructor.new("Chris")
+
+puts student.hello
+puts instructor.hello
