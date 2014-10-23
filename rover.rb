c31362db14be2cfff5629056c5795660522600a5
wduntak <wduntak@gmail.com>
2014-10-23 16:28:28 -0400

    Added a parameter to not allow rover to over extend input

--- rover.rb
+++ rover.rb
@@ -12,6 +12,12 @@
 0 1 2 3 4 5 6 7 8 9
 =end
 
+puts "What x-value do you want to input as a parameter?"
+$x_param = gets.chomp.to_i
+
+puts "What y-value do you want to input as a parameter?"
+$y_param = gets.chomp.to_i
+
 class Rover
 	attr_accessor(:x, :y, :direction, :instruction)
 
@@ -27,17 +33,22 @@ class Rover
 			elsif command == "R"
 				turn_right
 			elsif command == "M"
-				if grid_available 
+				if grid_availible? 
 					move
+				else
+					puts "Rover has fallen off the edge of the plateau! Goodbye!"
+					return
 				end
 			end
 		end
-
 	end
-	
-	def grid_available
-		if 
 
+	def grid_availible?
+		if (@x < $x_param && @y < $y_param)
+			true
+		else
+			false
+		end
 	end
 
 	def move
@@ -78,12 +89,6 @@ class Rover
 	end
 end
 
-puts "What x-value do you want to input as a parameter?"
-x_param = gets.chomp.to_i
-
-puts "What y-value do you want to input as a parameter?"
-y_param = gets.chomp.to_i
-
 puts "What point along 'x' would you like to being?"
 x_point = gets.chomp.to_i
 
@@ -94,10 +99,14 @@ puts "Which direction would you like to face?(N/E/S/W)"
 dir_point = gets.chomp.upcase.to_s
 
 puts "Where would you like to go? (Input 'M' to move forward, 'L' to rotate left, 'R' to rotate right)"
-add_move = gets.chomp.upcase.split(//)
+move_point = gets.chomp.upcase.split(//)
 
-rover1 = Rover.new(x_point, y_point, dir_point, move_point
+rover1 = Rover.new(x_point, y_point, dir_point, move_point)
 
 puts "Rover 1 is at (#{rover1.x}, #{rover1.y}) : #{rover1.direction}"
+
+
+
+
 
 
