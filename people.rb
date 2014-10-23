bdf5ee02612ef41b097205fed856516981f4f0af
wduntak <wduntak@gmail.com>
2014-10-23 11:04:35 -0400

    Called the teach and learn method on Student and Teacher classes.

--- people.rb
+++ people.rb
@@ -30,3 +30,14 @@ instructor = Instructor.new("Chris")
 
 puts student.hello
 puts instructor.hello
+
+puts instructor.teach
+puts student.learn
+
+puts student.teach
+
+=begin
+After calling the teach method on the student, an NoMethodError
+is returned.  Student cannot utilize the teach method because it 
+is not inherited from the Instructor class which contains the 
+teach method.
